c768c23d6e2071494cc36fe611dbbb20afe66602
Masha-Mascot-00 <mashamascot969@gmail.com>
2020-10-25 20:44:26 +0300

    feat: add array join from matrix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+  if (matrix === undefined) {
+    return [];
+  }else{
+    if (matrix.length) {
+        let array = [];
+        for (let index = 0; index < matrix.length; index++) {
+            const elementMatrix = matrix[index];
+            if (index % 2) {
+                elementMatrix.reverse();
+            }
+            elementMatrix.forEach((element) => {
+                array.push(element);
+            });
+        }
+        return array;
+    } else {
+        return matrix;
+    }
+  }
+    
+};
